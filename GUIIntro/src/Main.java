6a720c81324d0f434e57bd334fce3e6f8ef819d8
Doğukan Öztürk <125322507+hackInForeach@users.noreply.github.com>
2023-09-11 17:01:08 +0300

    Add files via upload

---
+++ GUIIntro/src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		String name=JOptionPane.showInputDialog("Enter your name");
+		JOptionPane.showMessageDialog(null, "Hello "+name);
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age "));
+		JOptionPane.showMessageDialog(null, "You'r  "+age+" years old");
+		
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height "));
+		JOptionPane.showMessageDialog(null,"You'r "+height+" cm tall");
+		
+		
+	}
+
+}
