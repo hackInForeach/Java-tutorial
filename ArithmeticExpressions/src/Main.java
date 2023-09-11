97a4d03c00cba653e9b411717794e2ecac6a4ab9
Doğukan Öztürk <125322507+hackInForeach@users.noreply.github.com>
2023-09-11 17:00:49 +0300

    Add files via upload

---
+++ ArithmeticExpressions/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// expression = operands & operators
+		// operands = values, variables,numbers,quantity
+		// operators = + - * / %
+		
+		double friends=10;
+		
+		friends =friends /3;
+		
+		System.out.println(friends);
+		
+	}
+
+}
