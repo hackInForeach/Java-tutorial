8cb66156d7b75a1c916e8acfbb51c41c188c6a09
Doğukan Öztürk <125322507+hackInForeach@users.noreply.github.com>
2023-09-11 17:02:03 +0300

    Add files via upload

---
+++ JavaAbstraction/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// abstract = abstract classes cannot be instantiated but they can have a subclass
+		//			  abstract methods are declared without an implementation
+		
+		//Vehicle vehicle = new Vehicle();
+		Car car = new Car();
+		car.go();
+	}
+
+}
---
+++ JavaAbstraction/src/Vehicle.java
@@ -0,0 +1,6 @@
+
+public abstract class Vehicle {
+
+	abstract void go();
+	
+}
