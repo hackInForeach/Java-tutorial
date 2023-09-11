ca35be995d74302904438aa118cf0c0adb8d7079
Doğukan Öztürk <125322507+hackInForeach@users.noreply.github.com>
2023-09-11 17:02:30 +0300

    Add files via upload

---
+++ JavaAccessModifiersPublicProtectedPrivate/src/package1/A.java
@@ -0,0 +1,13 @@
+package package1;
+
+import package2.*;
+
+public class A {
+	//protected String protectedMessage = "This is protected";
+
+	public static void main(String[] args) {
+
+		B b = new B();
+		//System.out.println(b.privateMessage);
+	}
+}
---
+++ JavaAccessModifiersPublicProtectedPrivate/src/package2/C.java
@@ -0,0 +1,10 @@
+package package2;
+import package1.*;
+
+public class C {
+
+	public      String publicMessage="This is public";
+	protected   String protectedMessage="This is protected";
+			    String defaultMessage="this is the default";
+	private     String privateMessage="this is private";
+}
